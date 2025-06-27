de5f8ef204f9126722f34348798a212a10d39a0b
165dsiaoh <3383661901@qq.com>
2025-06-27 11:23:11 +0800

    xiaoqi1.py

--- xiaoqi1.py
+++ xiaoqi1.py
@@ -49,8 +49,8 @@ with st.sidebar:
         ]
     model_name = st.selectbox(label ="请选择模型",options = model_options)
 
-        if model_name in {            ["gpt-3.5-turbo","gpt-3.5-turbo-0301","gpt-3.5-turbo-0613","gpt-3.5-turbo-16k",
-                                  "gpt-3.5-turbo-16k-0613","gpt-4", "gpt-4-0314","gpt-4-0613","gpt-4-32k",]}:
+        if model_name in {["gpt-3.5-turbo","gpt-3.5-turbo-0301","gpt-3.5-turbo-0613","gpt-3.5-turbo-16k",
+                                  "gpt-3.5-turbo-16k-0613","gpt-4", "gpt-4-0314","gpt-4-0613","gpt-4-32k"]}:
         api_key = st.secrets['API_KEY1']
     else:
         api_key = st.secrets['API_KEY2']
